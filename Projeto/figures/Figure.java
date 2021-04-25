073a9daf238b9b10816c93344a1a2e6d82c49dc5
pedro-lp2 <79579053+pedro-lp2@users.noreply.github.com>
2021-04-25 19:55:53 -0300

    Update Figure.java

--- Projeto/figures/Figure.java
+++ Projeto/figures/Figure.java
@@ -3,21 +3,39 @@ package figures;
 import java.awt.*;
 
 public abstract class Figure {
-    public int x, y;
-    public int w, h;
+    int x, y;
+    int w, h;
     Color line, background;
 
-    public Figure(int x, int y, int w, int h, Color line, Color background){
+    public Figure (int x, int y, int w, int h, Color line, Color background){
+    	
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
         this.line = line;
-        this.background=background;
+        this.background = background;
     }
 
     public abstract void print();
 
     public abstract void paint(Graphics g);
     
+    public int getx(){
+        return x;
+    }
+    public int gety(){
+        return y;
+    }
+    public int getw(){
+        return w;
+    }
+    public int geth(){
+        return h;
+    }
+    public void drag(int newx, int newy){
+        this.x = newx;
+        this.y = newy;
+    }
+
 }
