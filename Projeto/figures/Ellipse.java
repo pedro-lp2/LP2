8af26451cbba207b45c00cebd6997ed2e4c3ffd9
pedro-lp2 <79579053+pedro-lp2@users.noreply.github.com>
2021-05-30 20:15:51 -0300

    Update Ellipse.java

--- Projeto/figures/Ellipse.java
+++ Projeto/figures/Ellipse.java
@@ -1,35 +1,36 @@
 package figures;
-
+import java.awt.Graphics;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
-public class Ellipse extends Figure{
-    int x, y;
-    int w, h;
-    Color line, background;
-    Ellipse2D poly;
-
-    public Ellipse (int x, int y, int w, int h, Color line, Color background){
+public class Elipse extends Figure{
+    private static final long serialVersionUID = 1L;
+   
+    public Elipse (int x, int y, int w, int h, Color line, Color background){
         super(x, y, w, h, line, background);
-        this.x = x; 
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.line = line;
-        this.background = background;
-        
-        this.poly = new Ellipse2D.Double(this.x, this.y, this.w, this.h);
     }
-
-    public void print(){
+	
+	/** dimensões agora são private
+	public void print(){
         System.out.format("Elipse tamanho (%d, %d) // posicao(%d, %d).\n", this.w, this.h, this.x, this.y);
-    }
-    public void paint(Graphics g){
+    }**/
+	
+	
+    public void paint(Graphics g, boolean focused){
+    	Color Cfocus;
+    	if(focused == true) {
+    		Cfocus = Color.red;
+    	}
+    	else {
+    		Cfocus = new Color(0,0,0,0);
+    	}
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(this.line);
-        g2d.draw(this.poly);
-        g2d.setColor(this.background);
-        g2d.fill(this.poly);
+        g.setColor(this.getBorder());
+        g2d.draw(new Ellipse2D.Double(this.getX(),this.getY(),this.getW(),this.getH()));
+        g.setColor(this.getFill());
+        g2d.fill(new Ellipse2D.Double(this.getX(),this.getY(),this.getW(),this.getH()));
+        g.setColor(Cfocus);
+        g2d.draw(new Ellipse2D.Double(this.getX()-1, this.getY()-1, this.getW()+2, this.getH()+2));
     }
     
 }
