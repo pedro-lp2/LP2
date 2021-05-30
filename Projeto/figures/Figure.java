c221cae649fa6e77e303ce95e69f564a53642954
pedro-lp2 <79579053+pedro-lp2@users.noreply.github.com>
2021-05-30 20:22:22 -0300

    Update Figure.java

--- Projeto/figures/Figure.java
+++ Projeto/figures/Figure.java
@@ -2,42 +2,82 @@ package figures;
 import java.awt.*;
 import ivisible.IVisible;
 
-public abstract class Figure implements IVisible{
-    int x, y;
-    int w, h;
-    Color line, background;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.io.Serializable;
 
-    public Figure (int x, int y, int w, int h, Color line, Color background){
-    	
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.line = line;
-        this.background = background;
-    }
-
-    public abstract void print();
-
-    public abstract void paint(Graphics g);
+public  abstract class Figure implements IVisible, Serializable{
+	private static final long serialVersionUID = 1L;
+	private int x;
+	private int y;
+	private int w;
+	private int h;
+	private Color border;
+	private Color fill;
+	private Color contour;
+	
+	public Figure(int x, int y, int w, int h, Color border, Color fill) {
+		this.setX(x);
+		this.setY(y);
+		this.setW(w);
+		this.setH(h);
+		this.setBorder(border);
+		this.setFill(fill);
+	}
     
-    public int getx(){
-        return x;
+    public void drag(int dx, int dy) {
+    	this.x += dx;
+    	this.y += dy;
     }
-    public int gety(){
-        return y;
+	
+	public boolean clicked(int x, int y) {
+    	return (this.x <= x && x <= this.x + this.w && this.y <= y && y <= this.y + this.h);
     }
-    public int getw(){
-        return w;
+	
+	public void paint(Graphics g, boolean focused){
+        
     }
-    public int geth(){
-        return h;
-    }
-    public void drag(int newx, int newy){
-        this.x = newx;
-        this.y = newy;
-    }
-    public boolean clicked (int x, int y) {
-        return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
-    }
+	
+    public int getX() {
+		return this.x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return this.y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getW() {
+		return this.w;
+	}
+	public void setW(int w) {
+		this.w = w;
+	}
+	public int getH() {
+		return this.h;
+	}
+	public void setH(int h) {
+		this.h = h;
+	}
+	public Color getFill() {
+		return this.fill;
+	}
+	public void setFill(Color fill) {
+		this.fill = fill;
+	}
+	public Color getContour() {
+		return this.contour;
+	}
+	public void setContour(Color contour) {
+		this.contour = contour;
+	}
+	public Color getBorder() {
+		return this.border;
+	}
+	public void setBorder(Color border) {
+		this.border = border;
+	}
 }
