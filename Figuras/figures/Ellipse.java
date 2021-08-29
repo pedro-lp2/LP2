ecb8fa5652360c3daba1604d539fbd4873738d6a
pedro-lp2 <79579053+pedro-lp2@users.noreply.github.com>
2021-08-29 17:47:40 -0300

    Create Ellipse.java

---
+++ Figuras/figures/Ellipse.java
@@ -0,0 +1,35 @@
+package figures;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
+public class Ellipse {
+    int x, y;
+    int w, h;
+    Color line, background;
+    Ellipse2D poly;
+    public Ellipse (int x, int y, int w, int h, Color line, Color background){
+
+        this.x = x; 
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.line = line;
+        this.background = background;
+        
+        this.poly = new Ellipse2D.Double(this.x, this.y, this.w, this.h);
+    }
+
+    public void print(){
+        System.out.format("Elipse tamanho (%d, %d) // posicao(%d, %d).\n", this.w, this.h, this.x, this.y);
+    }
+    public void paint(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(this.line);
+        g2d.draw(this.poly);
+        g2d.setColor(this.background);
+        g2d.fill(this.poly);
+    }
+
+    
+}
