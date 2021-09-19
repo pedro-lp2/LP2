9e3a0957f5563613c851cd46919424c3479d6edd
pedro-lp2 <79579053+pedro-lp2@users.noreply.github.com>
2021-09-19 19:49:51 -0300

    Create Ellipse.java

---
+++ 03-Figuras/figures/Ellipse.java
@@ -0,0 +1,22 @@
+package figures;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
+
+public class Ellipse extends Figure{
+    int x, y;
+    int w, h;
+    
+    public Ellipse (int x, int y, int w, int h){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+       
+    }
+    public void paint(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.draw(new Ellipse2D.Double(this.x,this.y,this.w,this.h));
+    }
+}
