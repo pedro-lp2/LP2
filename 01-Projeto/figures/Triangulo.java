ddace19b3173fe35112bf346870dbc49153b92ee
pedro-lp2 <79579053+pedro-lp2@users.noreply.github.com>
2021-10-24 15:26:15 -0300

    Update Triangulo.java

--- 01-Projeto/figures/Triangulo.java
+++ 01-Projeto/figures/Triangulo.java
@@ -1,42 +1,35 @@
 package figures;
-
+import java.awt.Graphics;
 import java.awt.*;
 
 public class Triangulo extends Figure{
-    int x, y;
-    int w, h;
-    Color line, background;
-    Polygon triangulo = new Polygon();
+	private static final long serialVersionUID = 1L;
+	public Polygon polygon;
 
-    public Triangulo (int x, int y, int w, int h, Color line, Color background){
+	public Triangulo (int x, int y, int w, int h, Color line, Color background){
         super(x, y, w, h, line, background);
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = 0;
-        this.line = line;
-        this.background = background;
-
-        triangulo.npoints = 3;
-        triangulo.xpoints[0] = x;
-        triangulo.xpoints[1] = y;
-        triangulo.xpoints[2] = w;
-
-        triangulo.ypoints[0] = y;
-        triangulo.ypoints[1] = w;
-        triangulo.ypoints[2] = y;
-
     }
-
-    public void print(){
-        System.out.format("Triangulo de base (%d, %d)", this.x, this.y);
+    public void paint(Graphics g, boolean focused){
+    	
+    	int x_array[] = {this.getX(), this.getX() + this.getW()/2, this.getX() + this.getW()};
+        int y_array[] = {this.getY() + this.getH(), this.getY(), this.getY() + this.getH()};
+        Polygon polygon = new Polygon(x_array, y_array, 3);
+        this.polygon = polygon;
+        
+        Color Cfocus;
+    	if(focused == true) {
+    		Cfocus = Color.red;
+    	}
+    	else {
+    		Cfocus = new Color(0,0,0,0);
+    	}
+        Graphics2D g2d = (Graphics2D) g;
+        g.setColor(this.getBorder());
+        g2d.drawPolygon(this.polygon);
+        g.setColor(this.getFill());
+        g2d.fillPolygon(this.polygon);
+        g.setColor(Cfocus);
+        g2d.drawPolygon(this.polygon);
     }
     
-    public void paint(Graphics g){
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(this.line);
-        g2d.drawPolygon(triangulo.xpoints, triangulo.ypoints, triangulo.npoints);
-        g2d.setColor(this.background);
-        g2d.fill(triangulo);
-    }
 }
