23c10998163990ba969652dd1aae92d592b6f9a6
pedro-lp2 <79579053+pedro-lp2@users.noreply.github.com>
2021-11-07 22:46:13 -0300

    Create BRetangulo.java

---
+++ 01-Projeto/buttons/BRetangulo.java
@@ -0,0 +1,33 @@
+package buttons;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import figures.Figure;
+
+public class BRetangulo extends Button{
+
+	public BRetangulo(int idx, Figure fig) {
+		super(idx, fig);
+	}
+	
+	public void paint(Graphics g, boolean focused) {
+        Color CbackGround;
+    	if(focused == true) {
+    		CbackGround = Color.gray;
+    	}
+    	else {
+    		CbackGround = Color.white;
+    	}
+    	
+    	
+        Graphics2D g2d = (Graphics2D) g;
+        g.setColor(CbackGround);
+        g2d.fillRect(super.getFig().getX() - 5, super.getFig().getY() - 5, super.getFig().getW() + 10, super.getFig().getH() + 10);
+        g2d.setColor(super.getFig().getFill());
+        
+        g2d.drawRect(super.getFig().getX() - 5, super.getFig().getY() - 5, super.getFig().getW() + 10, super.getFig().getH() + 10);
+        g2d.setColor(super.getFig().getFill());
+        g2d.fillRect(super.getFig().getX(), super.getFig().getY(), super.getFig().getW(), super.getFig().getH());
+	}
+}
