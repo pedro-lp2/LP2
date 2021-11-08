842fc861759881af040e2df4aceacca60446a136
pedro-lp2 <79579053+pedro-lp2@users.noreply.github.com>
2021-11-07 22:22:38 -0300

    Update App.java

--- 01-Projeto/App.java
+++ 01-Projeto/App.java
@@ -1,6 +1,15 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+
+import buttons.BDeletar;
+import buttons.BElipse;
+import buttons.BMudaCor;
+import buttons.BPentagono;
+import buttons.BRetangulo;
+import buttons.BTriangulo;
+import buttons.Button;
+
 import java.util.ArrayList;
 import java.util.Random;
 import figures.*;
@@ -27,9 +36,9 @@ class Frame extends JFrame{
 	ArrayList<Button> buts = new ArrayList<Button>();
     
 	Random rand = new Random();
-    Figure Ffocus = null;
-    Button Bfocus = null;
-    Figure ColorAux = null;
+    Figure ffocus = null;
+    Button bfocus = null;
+    Figure colorAux = null;
     
     int aux = 0;
     int colorPaintAux = 0;
@@ -49,6 +58,12 @@ class Frame extends JFrame{
     		JOptionPane.showMessageDialog(null, "Erro 101",
     			      "Erro!", JOptionPane.ERROR_MESSAGE);
     	}
+		buts.add(new BRetangulo(1, new Retangulo(20, 50, 30, 30, Color.BLACK, Color.BLACK)));
+    	buts.add(new BElipse(2, new Elipse(20,90,30,30, Color.BLACK, Color.BLACK)));
+    	buts.add(new BTriangulo(3, new Triangulo(20,130,30,30, Color.BLACK, Color.BLACK))); 
+    	buts.add(new BPentagono(5, new Pentagono(20,170,30,30, Color.BLACK, Color.BLACK)));
+    	buts.add(new BDeletar(7, new Retangulo(20, 240, 70, 20, Color.BLACK, Color.BLACK))); // Deleta figura em foco
+    	buts.add(new BMudaCor(9, new Elipse(20, 270, 70, 20, Color.BLACK, Color.BLACK))); // Mudar a Cor da figura
         this.addWindowListener(
             new WindowAdapter(){
                 public void windowClosing(WindowEvent e){
@@ -88,57 +103,57 @@ class Frame extends JFrame{
                     } else if(evt.getKeyChar() == 'p') {
                         figs.add(new Pentagono(x,y, w,h, Color.white,Color.black));                   
                     }else if(evt.getKeyCode() == KeyEvent.VK_DELETE){
-                    	figs.remove(Ffocus);
+                    	figs.remove(ffocus);
                     } else if(evt.getKeyChar() == '+'){
-                    	if(Ffocus.getW() < 400 && Ffocus.getH() < 400) {
-                        	Ffocus.setH(Ffocus.getH() + Ffocus.getH()/10);
-                        	Ffocus.setW(Ffocus.getW() + Ffocus.getW()/10);
-                            if(Ffocus.getH()/10 == 0 || Ffocus.getW()/10 == 0) {
-                            	Ffocus.setH(Ffocus.getH() + 1);
-                            	Ffocus.setW(Ffocus.getW() + 1);
+                    	if(ffocus.getW() < 400 && ffocus.getH() < 400) {
+                        	ffocus.setH(ffocus.getH() + ffocus.getH()/10);
+                        	ffocus.setW(ffocus.getW() + ffocus.getW()/10);
+                            if(ffocus.getH()/10 == 0 || ffocus.getW()/10 == 0) {
+                            	ffocus.setH(ffocus.getH() + 1);
+                            	ffocus.setW(ffocus.getW() + 1);
                             }
                         }
                     } else if(evt.getKeyChar() == '-'){
-                    	if(Ffocus.getW() > 0 && Ffocus.getH() > 0) {
-                    		Ffocus.setH(Ffocus.getH() - Ffocus.getH()/10);
-                        	Ffocus.setW(Ffocus.getW() - Ffocus.getW()/10);
+                    	if(ffocus.getW() > 0 && ffocus.getH() > 0) {
+                    		ffocus.setH(ffocus.getH() - ffocus.getH()/10);
+                        	ffocus.setW(ffocus.getW() - ffocus.getW()/10);
                     	}
                     } else if(evt.getKeyCode() == KeyEvent.VK_UP){
-                    	Ffocus.setY(Ffocus.getY() - 1);
+                    	ffocus.setY(ffocus.getY() - 1);
                     } else if(evt.getKeyCode() == KeyEvent.VK_DOWN){
-                    	Ffocus.setY(Ffocus.getY() + 1);
+                    	ffocus.setY(ffocus.getY() + 1);
                     } else if(evt.getKeyCode() == KeyEvent.VK_LEFT){
-                    	Ffocus.setX(Ffocus.getX() - 1); 
+                    	ffocus.setX(ffocus.getX() - 1); 
                     } else if(evt.getKeyCode() == KeyEvent.VK_RIGHT){
-                    	Ffocus.setX(Ffocus.getX() + 1);
+                    	ffocus.setX(ffocus.getX() + 1);
                     } else if(evt.getExtendedKeyCode() == KeyEvent.VK_SPACE){
                     	if(figs.size() > 0) {
-                    		if(Ffocus == null) {
-                    			Ffocus = figs.get(0);
+                    		if(ffocus == null) {
+                    			ffocus = figs.get(0);
                     		}
-                    		Ffocus = null;
+                    		ffocus = null;
                         	if(aux == 0) {
-                        		Ffocus = figs.get(figs.size() - 1);
+                        		ffocus = figs.get(figs.size() - 1);
                         	}
                         	if(aux != 0) {
-                        		Ffocus = figs.get(aux - 1);
+                        		ffocus = figs.get(aux - 1);
                         	}
-                        	Ffocus = null;
+                        	ffocus = null;
                         	if(aux >= figs.size()) {
                         		aux = 0;
                         	}
-                        	Ffocus = figs.get(aux);
-                        	Ffocus.setContour(Color.red);
+                        	ffocus = figs.get(aux);
+                        	ffocus.setContour(Color.red);
                         	aux += 1;
                     	}
                     } else if(evt.getKeyChar() == 'f'){
-                    	Ffocus.setFill(colors[colorPaintAux]);
+                    	ffocus.setFill(colors[colorPaintAux]);
                     	colorPaintAux += 1;
                     	if(colorPaintAux >= 6) {
                     		colorPaintAux = 0;
                     	}
                     } else if(evt.getKeyChar() == 'c'){
-                    	Ffocus.setBorder(colors[colorPaintAux]);
+                    	ffocus.setBorder(colors[colorPaintAux]);
                     	colorPaintAux += 1;
                     	if(colorPaintAux >= 6) {
                     		colorPaintAux = 0;
@@ -152,31 +167,85 @@ class Frame extends JFrame{
         this.addMouseMotionListener(
         	new MouseMotionAdapter() {
         		public void mouseDragged(MouseEvent evt) {
-        			int dif_x = (evt.getX() - Ffocus.getX()) - Ffocus.getW()/2;
-        			int dif_y = (evt.getY() - Ffocus.getY()) - Ffocus.getH()/2;
-        			Ffocus.drag(dif_x, dif_y);
+        			int dif_x = (evt.getX() - ffocus.getX()) - ffocus.getW()/2;
+        			int dif_y = (evt.getY() - ffocus.getY()) - ffocus.getH()/2;
+        			ffocus.drag(dif_x, dif_y);
         			repaint();
         		}
         	}
         );
         this.addMouseListener(
-                new MouseAdapter() {
-            	public void mousePressed(MouseEvent evt) {
-            	    Ffocus = null;
-            	    for(Figure fig: figs) {
-            		if(fig.x <= evt.getX() && fig.y <= evt.getY() && (fig.w + fig.x)>= evt.getX() && (fig.h + fig.y) >= evt.getY()) {
-    	        	   Ffocus = fig;
-    	    		    Ffocus.contour = Color.red;
-    	    		    repaint();
-            		} else {
-            		    fig.contour = null;
+            	new MouseAdapter() {
+            		public void mousePressed(MouseEvent evt) {
+            			boolean auxClick = false;
+            			for(Button but: buts) {
+            				if(but.clicked(evt.getX(),evt.getY())) {
+    	        				bfocus = but;
+    	        				if(but.getIdx() == 7 && ffocus != null) {
+    								figs.remove(ffocus);
+    								bfocus = null;
+    								repaint();
+    							}
+    							if(but.getIdx() == 8 && !figs.isEmpty()) {
+    								figs.clear();
+    								bfocus = null;
+    								repaint();
+    							}
+    							if(but.getIdx() == 9 && ffocus != null) {
+    								ffocus.setFill(colors[colorPaintAux]);
+    		                    	colorPaintAux += 1;
+    		                    	if(colorPaintAux >= 12) {
+    		                    		colorPaintAux = 0;
+    		                    	}
+    		                    	bfocus = null;
+    		                    	repaint();
+    							}
+    	    					repaint();
+    	    					auxClick = true;
+            				}		
+            			}
+            			if(bfocus != null && !(bfocus.clicked(evt.getX(),evt.getY()))) {
+    						if(bfocus.getIdx() == 1) {
+    							figs.add(new Retangulo(evt.getX(),evt.getY(), 30,30, Color.white,Color.black));  
+    							bfocus = null;
+    						}
+    						if (bfocus != null) {
+	    						if(bfocus.getIdx() == 2) {
+	    							figs.add(new Elipse(evt.getX(),evt.getY(), 30,30, Color.white,Color.black));
+	    							bfocus = null;
+	    						}
+	    						if (bfocus != null) {
+		    						if(bfocus.getIdx() == 3) {
+		    							figs.add(new Triangulo(evt.getX(),evt.getY(), 30,30, Color.white,Color.black));
+		    							bfocus = null;
+		    						}
+		    						if (bfocus != null) {
+			    						if(bfocus.getIdx() == 5) {
+			    							figs.add(new Pentagono(evt.getX(),evt.getY(), 30,30, Color.white,Color.black));
+			    							bfocus = null;
+			    						}
+		    						}
+	    						}
+    						}
+            			}
+            			
+            			for(Figure fig: figs) {
+            				if(fig.clicked(evt.getX(),evt.getY())) {
+    	        				ffocus = fig;
+    	        				figs.remove(fig);
+    	        				figs.add(ffocus);
+    	    					ffocus.setContour(Color.red);
+    	    					repaint();
+    	    					break;
+            				} else {
+            					if(auxClick == false) {
+            						ffocus = null;
+            					}
+            				}
+            			}
+            			repaint();
             		}
-            	    }
-            	    if(Ffocus == null) {
-            	        repaint();
-            	    }
             	}
-                }
             );
         		/**JANELA**/
         setDefaultCloseOperation(EXIT_ON_CLOSE);	
@@ -191,11 +260,19 @@ class Frame extends JFrame{
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.dispose();
         for(Figure fig: this.figs){
-        	if(fig == Ffocus) {
+        	if(fig == ffocus) {
         		fig.paint(g, true);
         	}
         	else {
         		fig.paint(g, false);
+        	}
+        }
+        for(Button but: this.buts){
+        	if(but == bfocus) {
+        		but.paint(g, true);
+        	}
+        	else {
+        		but.paint(g, false);
         	}
         }
     }
