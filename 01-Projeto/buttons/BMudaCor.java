1c35618f9d461a3235f9e31c1b01d7eb68d9aca0
pedro-lp2 <79579053+pedro-lp2@users.noreply.github.com>
2021-11-07 22:43:05 -0300

    Create BMudaCor.java

---
+++ 01-Projeto/buttons/BMudaCor.java
@@ -0,0 +1,39 @@
+package buttons;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import figures.Figure;
+
+public class BMudaCor extends Button{
+
+	public BMudaCor(int idx, Figure fig) {
+		super(idx, fig);
+	}
+	
+	public void paint(Graphics g, boolean focused) {
+        Color CbackGround;
+    	if(focused == true) {
+    		CbackGround = Color.gray;
+    	}
+    	else {
+    		CbackGround = Color.white;
+    	}
+        Graphics2D g2d = (Graphics2D) g;
+        g.setColor(CbackGround);
+        g2d.fillRect(super.getFig().getX()-5, super.getFig().getY()-5, super.getFig().getW()+10, super.getFig().getH()+10);
+        g2d.setColor(super.getFig().getFill());
+        
+        g2d.drawRect(super.getFig().getX()-5, super.getFig().getY()-5, super.getFig().getW()+10, super.getFig().getH()+10);
+        g2d.setColor(super.getFig().getFill());
+        
+        int fontSize = 20;
+        g2d.setFont(new Font("TimesRoman", Font.PLAIN, fontSize));
+        g2d.setColor(Color.red);
+        
+        g2d.drawString("Cor", super.getFig().getX()+7, super.getFig().getY()+15);
+	}
+
+}
